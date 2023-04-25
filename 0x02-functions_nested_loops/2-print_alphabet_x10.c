8eb1827aea518d36c22194efd0d2562be635d2ce
IlhameYagou <i.yagou28@gmail.com>
2023-04-25 09:32:42 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,21 @@
  * print_alphabet_x10 - alphabetx10
  * return: void
  */
+
 void print_alphabet_x10(void)
 {
 
 	char ch;
 	int i;
 	i = 0;
-		while (i < 10)
-		{
-
+	while (i < 10)
+	{
 	for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			_putchar(ch);
-		}
-		_putchar('\n');
-		i++;
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	i++;
 	}
 }
 
