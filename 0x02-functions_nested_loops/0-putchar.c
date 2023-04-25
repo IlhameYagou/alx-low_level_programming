17e779ab4b594be135ae6f108dc2d435e1c17513
IlhameYagou <i.yagou28@gmail.com>
2023-04-25 06:18:52 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * description: 'a program that prints a sentence'
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+		return (0);
+}
