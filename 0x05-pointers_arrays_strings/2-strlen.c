fcc7d7fb4271b9ece0c2b25d52aa621d81620eaf
IlhameYagou <i.yagou28@gmail.com>
2023-05-02 04:36:47 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ *@s: string to be checked
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+
+	int string_length = O;
+
+	while (s[string_length])
+		string_length++;
+	return (string_length);
+}
