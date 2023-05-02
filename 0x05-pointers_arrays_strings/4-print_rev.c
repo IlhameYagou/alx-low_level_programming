41ffa6719206c5eb74bd458ab9fd3b9d80459f25
IlhameYagou <i.yagou28@gmail.com>
2023-05-02 04:57:25 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string reference pointer used
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int start = 0;
+
+	while (s[start])
+		start++;
+	while (start--)
+		_putchar(s[start]);
+	_putchar('\n');
+}
