0fb252d5afc26e5f26611524882b68a4d222e936
IlhameYagou <i.yagou28@gmail.com>
2023-05-12 08:58:48 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always zero
+ */
+
+int main(intargc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
