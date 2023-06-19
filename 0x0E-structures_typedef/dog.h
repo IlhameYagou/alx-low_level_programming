aa06c13387ced289f9932dc9d042361fc33735cf
IlhameYagou <i.yagou28@gmail.com>
2023-06-19 14:11:02 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - poppy's structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: Poppy's Structure
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
