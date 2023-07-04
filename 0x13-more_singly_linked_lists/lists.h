80087539f8f5a4f5761a64162d6e76b4e86e4c6f
IlhameYagou <i.yagou28@gmail.com>
2023-07-04 14:55:15 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -17,7 +17,7 @@ typedef struct listint_s
 {
     int n;
     struct listint_s *next
-} listint_t;
+}listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
